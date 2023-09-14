8ba30ad80070fa39a2b416c0c7faa91f966496f6
Documenter.jl <documenter@juliadocs.github.io>
2023-09-14 17:03:02 +0000

    build based on 7f0d5f1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RationalFunctionApproximation","category":"page"},{"location":"#RationalFunctionApproximation","page":"Home","title":"RationalFunctionApproximation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RationalFunctionApproximation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RationalFunctionApproximation]","category":"page"}]
+}
