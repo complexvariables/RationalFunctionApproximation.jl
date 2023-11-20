684fae884693bd81bc7894f0f40a0b414e45139a
Toby Driscoll <driscoll@udel.edu>
2023-11-20 13:57:03 -0500

    avoid overwriting methods in extensions

--- src/RationalFunctionApproximation.jl
+++ src/RationalFunctionApproximation.jl
@@ -24,12 +24,12 @@ include("operations.jl")
 # These are overloaded by plotting extensions.
 export convergenceplot, errorplot
 
-function convergenceplot(r::Union{Barycentric,Approximation})
-    @error "Load the Makie package to make a convergence plot"
+function convergenceplot()
+    @error "Load the Makie or Plots package to make a convergence plot"
 end
 
-function errorplot(r::Approximation; args...)
-    @error "Load the Makie package to make an error plot"
+function errorplot()
+    @error "Load the Makie or Plots package to make an error plot"
 end
 
 end
