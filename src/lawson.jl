4614b067a470c1349896436c1da124b293e238c3
Toby Driscoll <driscoll@udel.edu>
2024-07-02 14:33:05 -0400

    add example to minimax doc

--- src/lawson.jl
+++ src/lawson.jl
@@ -28,6 +28,20 @@ end
 Compute an approximately minimax rational approximation to a function `f` on the nodes of a
 given rational function in barycentric form. The returned approximation has the same type
 as the first input argument.
+
+The `nsteps` argument controls the number of Lawson iterations. The default value is 20.
+
+# Examples
+```jldoctest
+julia> f(x) = tanh( 40*(x - 0.15) );
+julia> r = approximate(f, unit_interval, max_degree=8);  # least-squares approximation
+julia> check(r);
+[ Info: Max error is 1.06e-02
+
+julia> r̂ = minimax(r);
+julia> check(r̂);
+[ Info: Max error is 1.40e-03
+```
 """
 function minimax(r::Barycentric, f::Function, nsteps::Integer=20)
     node, val, weight = nodes(r), values(r), weights(r)
